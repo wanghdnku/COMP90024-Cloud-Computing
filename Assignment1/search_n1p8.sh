5d65e95e32038173120282d1a4608b464cc616bc
Haidong Wang <wanghd.nku@outlook.com>
2016-04-01 15:30:42 +1100

    修改了shell script

--- Assignment1/search_n1p8.sh
+++ Assignment1/search_n1p8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=8
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=1
 module load Python/3.4.3-goolf-2015a
 srun search_mpi.py
--- Assignment1/search_n2p4.sh
+++ Assignment1/search_n2p4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --nodes=2
-#SBATCH --ntasks=8
-#SBATCH --cpus-per-task=4
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=1
 module load Python/3.4.3-goolf-2015a
 srun search_mpi.py
