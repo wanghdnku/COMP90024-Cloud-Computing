c7e48e8f10a8ef346e1b3a0018faa44083fe856d
Haidong Wang <wanghd.nku@outlook.com>
2016-04-03 12:41:17 +1000

    删除了以前的版本，添加了注释

--- Assignment1/search_mpi.py
+++ Assignment1/search_mpi.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# ============================================================================ #
+#  File: search_mpi.py                                                         #
+#  Author: Haidong Wang                                                        #
+#  Student ID: 678725                                                          #
+#  Input: This program can receive one string parameter in the console, this   #
+#    parameter will be used to count it's frequency it appears in files.       #
+#  Output: The search result will be print in the console, along with the wall #
+#    time that the program costs.                                              #
+#  Description: This is the program that search a csv file in parallel system. #
+#    It can 1. find the frequency of given word, 2. find the top 10 popular    #
+#    users, 3. find the top 10 popular topics.                                 #
+# ============================================================================ #
+
+
 # import some module used in this program.
 import re
 import sys
@@ -8,7 +22,11 @@ from mpi4py import MPI
 from datetime import datetime
 from collections import Counter
 
+# ---------------------------------------------------------------------------- #
 
+
+# This function will divede a twitter file into a list of lines, the size of
+# the list is appointed by the parameter: piece_size.
 def divide_file(file, piece_size = 1024 * 1024 * 50):
     # Divide file by lines. Each piece is default size.
     while True:
@@ -17,9 +35,11 @@ def divide_file(file, piece_size = 1024 * 1024 * 50):
             break
         yield file_lines
 
+# ---------------------------------------------------------------------------- #
+
 
 # Record the beginning time.
-beginning = datetime.now().timestamp()
+program_start_time = datetime.now().timestamp()
 
 # Set the query word, if users input words from console, then replace it.
 query = 'melbourne'
@@ -33,7 +53,7 @@ rank = comm.Get_rank()
 mode = MPI.MODE_RDONLY
 
 # Set the path of twitter file, then open it.
-path = 'twitter.csv'
+path = '1GTwitter.csv'
 # Open the file.
 with open(path, 'r', encoding='utf-8') as twitter_file:
 
@@ -43,14 +63,17 @@ with open(path, 'r', encoding='utf-8') as twitter_file:
         sum_users = Counter()
         sum_topic = Counter()
 
+    # Timer count, this is a flag that guarantee the parallel time is only
+    # recorded once.
+    timer_count_start = True
+    timer_count_end = True
+    parallel_start_time = datetime.now().timestamp()
+    parallel_end_time = datetime.now().timestamp()
+
     # Divide large file into blocks
     for twitter_list in divide_file(twitter_file):
         # Do different tasks in different rank.
         if rank == 0:
-            # Read twitter file as a list of twitters, each element of this list
-            # is a twitter. The header of twitter file is also been moved.
-            # twitter_list = block
-            # del twitter_list[0]
             # Create a now twitter_chunks which is a list of list.
             twitter_chunks = [[] for _ in range(size)]
             for i, chunk in enumerate(twitter_list):
@@ -62,6 +85,11 @@ with open(path, 'r', encoding='utf-8') as twitter_file:
         # Each rank get their data from scatter.
         local_chunk = comm.scatter(twitter_chunks, root=0)
 
+        # Start record the time that program start running in parallel
+        if timer_count_start:
+            parallel_start_time = datetime.now().timestamp()
+            timer_count_start = False
+
         # Create 3 counters to record statistical data.
         query_per_chunk = Counter()
         users_per_chunk = Counter()
@@ -69,12 +97,21 @@ with open(path, 'r', encoding='utf-8') as twitter_file:
 
         # Search each line of chunk, update counters.
         for item in local_chunk:
+            # This regex will find all 'query word' in one twitter.
             queryPerItem = re.findall(query, item.lower())
+            # This regex will find all 'user'(start with @) in one twitter.
             usersPerItem = re.findall(r'(?<=@)\w+', item.lower())
+            # This regex will find all 'topic'(start with #) in one twitter.
             topicPerItem = re.findall(r'(?<=#)\w+', item.lower())
+            # Store the results in counters.
             query_per_chunk.update(queryPerItem)
             users_per_chunk.update(usersPerItem)
             topic_per_chunk.update(topicPerItem)
+
+        # Start record the end time that program running in parallel
+        if timer_count_end:
+            parallel_end_time = datetime.now().timestamp()
+            timer_count_end = False
 
         # Gathering data as a tuple.
         local_data = (query_per_chunk, users_per_chunk, topic_per_chunk)
@@ -88,10 +125,13 @@ with open(path, 'r', encoding='utf-8') as twitter_file:
                 sum_users.update(data_tuple[1])
                 sum_topic.update(data_tuple[2])
 
-######### Close the File
+#### The file is now been closed.
 
 # Record the end time.
-ending = datetime.now().timestamp()
+program_end_time = datetime.now().timestamp()
+
+# ---------------------------------------------------------------------------- #
+
 
 # Printing the data and formatting.
 if rank == 0:
@@ -124,6 +164,9 @@ if rank == 0:
             dotFormat -= 1
         print(' %s times' % times)
 
-    # Calculating the duration time.
-    duration = str("%.2f" % (ending - beginning))
-    print('\n[ Duration of work: %s s ]' % duration)
+    # Calculating the time of wall.
+    duration_time = str("%.2f" % (program_end_time - program_start_time))
+    # Calculating the time of program running in the parallel process.
+    parallel_time = str("%.2f" % (parallel_end_time - parallel_start_time))
+    print('\n[ Time of wall     : %s s ]' % duration_time)
+    print('\n[ Time of parallel : %s s ]' % parallel_time)
---
+++ Assignment1/timing_search.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --time=01:00:00
+#SBATCH --mem 10240
+#SBATCH --begin=16:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+module load Python/3.4.3-goolf-2015a
+srun search_mpi.py
